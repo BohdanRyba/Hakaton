cd9786ea1f23b623b281715fb79376569ab38998
SLot <anriy.slotvinskiy@gmail.com>
2016-10-14 18:13:15 +0300

    update page create event(ajax)

--- service/create-event.php
+++ service/create-event.php
@@ -2,88 +2,68 @@
     sleep(1);
 ?>
 <div class="box-header with-border">
-  <h2 class="box-title">Заполните онформацию о клубе</h2>
+  <h2 class="box-title">Создание события</h2>
 </div>
 <!-- /.box-header -->
 <!-- form start -->
 <form class="form-horizontal">
   <div class="box-body">
     <div class="form-group">
-      <label for="inputEmail3" class="col-sm-2 control-label">Название клуба</label>
+      <label for="event_name" class="col-sm-2 control-label">Название события</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputEmail3" placeholder="Название клуба">
+        <input type="text" class="form-control" id="event_name" placeholder="Введите информацию">
       </div>
     </div>
     <div class="file_dw form-group">
-        <label for="exampleInputFile">Загрузите логотип клуба</label>
-        <input id="exampleInputFile" type="file">
+      <label for="event_pic">Добавить афишу события</label>
+      <input id="event_pic" type="file">
     </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label">Страна</label>
-        <div class="col-sm-10">
-            <select class="form-control select2" style="width: 100%;">
-              <option selected="selected">Украина</option>
-              <option>Россия</option>
-              <option>США</option>
-              <option>Польша</option>
-              <option>Германия</option>
-              <option>Франция</option>
-              <option>Китай</option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Город</label>
-          <div class="col-sm-10">
-              <select class="form-control select2" style="width: 100%;">
-                <option selected="selected">Хмельницкий</option>
-                <option>Киев</option>
-                <option>Харковь</option>
-                <option>Днепропетровск</option>
-                <option>Донецк</option>
-                <option>Львов</option>
-                <option>Житомер</option>
-              </select>
-            </div>
-          </div>
     <div class="form-group">
-      <label for="inputPassword3" class="col-sm-2 control-label">Руководитель клуба</label>
+      <label for="event_status" class="col-sm-2 control-label">Статус</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputPassword3" placeholder="Руководитель клуба">
+        <input type="text" class="form-control" id="event_status" placeholder="Вид соревнований конкурса/мероприятия">
       </div>
     </div>
     <div class="form-group">
-      <label for="inputPassword3" class="col-sm-2 control-label">Тренер №1</label>
+      <label for="event_begin" class="col-sm-2 control-label">Дата начала</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputPassword3" placeholder="Тренер №1">
+        <input type="text" class="form-control" id="event_begin" placeholder="Выберите дату">
       </div>
     </div>
     <div class="form-group">
-      <label for="inputPassword3" class="col-sm-2 control-label">Тренер №2</label>
+      <label for="event_end" class="col-sm-2 control-label">Дата окончания</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputPassword3" placeholder="Тренер №2">
+        <input type="text" class="form-control" id="event_end" placeholder="Выберите дату">
       </div>
     </div>
     <div class="form-group">
-      <label for="inputPassword3" class="col-sm-2 control-label">Тренер №3</label>
+      <label for="event_city" class="col-sm-2 control-label">Выбирите город</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputPassword3" placeholder="Тренер №3">
+        <input type="text" class="form-control" id="event_city" placeholder="Введите город">
       </div>
     </div>
     <div class="form-group">
-      <label for="inputPassword3" class="col-sm-2 control-label">Номер телефона</label>
+      <label for="event_country" class="col-sm-2 control-label">Выбирите страну</label>
       <div class="col-sm-10">
-        <input type="tel" class="form-control" id="inputPassword3" placeholder="Номер телефона">
+        <input type="text" class="form-control" id="event_country" placeholder="Введите страну">
       </div>
     </div>
-    <div class="form-group">
-      <label for="inputPassword3" class="col-sm-2 control-label">Електроная почта</label>
-      <div class="col-sm-10">
-        <input type="e-mail" class="form-control" id="inputPassword3" placeholder="e-mail">
-      </div>
+
+  <div class="form-group">
+    <label for="event_sud" class="col-sm-2 control-label">Главный судья</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="event_sud" placeholder="Введите информацию">
     </div>
   </div>
-</form>
-<form action="" class="form_in">
-    <label class="" for=""><input name="reg_participant" type="submit" value="Заповнити заявку на участь"></label>
+  <div class="form-group">
+    <label for="event_skyt" class="col-sm-2 control-label">Скутинер</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="event_skyt" placeholder="Введите данные">
+    </div>
+  </div>
+  <div class="form_in">
+      <label class="" for=""><input name="reg_participant" type="submit" value="Принять"></label>
+  </div>
+  </div>
+</div>
 </form>
