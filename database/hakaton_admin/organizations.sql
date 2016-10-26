fb9b8deac2de2ac14a863959e00417a85d75d37e
RomaSlobodeniuk <romsl1991@gmail.com>
2016-10-26 21:21:59 +0300

    Rewrited databases

--- database/hakaton_admin/organizations.sql
+++ database/hakaton_admin/organizations.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 23 2016 г., 16:46
+-- Время создания: Окт 26 2016 г., 20:14
 -- Версия сервера: 10.1.9-MariaDB
 -- Версия PHP: 5.6.15
 
@@ -43,7 +43,11 @@ CREATE TABLE `organizations` (
 --
 
 INSERT INTO `organizations` (`id`, `org_name`, `org_abbreviation`, `org_head_fio`, `org_city`, `org_country`, `org_phone`, `org_email`, `org_pic_path`) VALUES
-(1, 'First Organization', 'FO', 'Head of the first organization', 'Kiev', 'Ukraine', 804424735265, 'first@gmail.com', 'pic_path');
+(1, 'First Organization', 'FO', 'Head of the first organization', 'Kiev', 'Ukraine', 804424735265, 'first@gmail.com', 'views\\main\\img\\org_image\\Dancing-300x300.jpg'),
+(2, 'Second Organization''s name', 'SO', 'Michael Jackson', 'Rio-de-Janejro', 'Brasil', 38067382258, 'michael_jackson@gmail.com', 'views\\main\\img\\org_image\\Blues-Dancing-300x300.jpg'),
+(3, 'Third Organization', 'THO', 'Emilia Clarck', 'City of the Dragon', 'Mifia', 80256485232, 'dragons@gmail.com', 'views/main/img/org_image/pic.jpg'),
+(4, 'Julia Roberts Organization', 'JRO', 'Julia Roberts', 'San-Francisko', 'USA', 448852135135, 'juli_rob@gmail.com', 'views/main/img/org_image/Julia-Roberts-150x150.jpg'),
+(5, 'Оля Корпорейшен', 'ОК', 'Гордова Ольга', 'Нежин', 'Украина', 80654735535, 'olia@gmail.com', 'views/main/img/org_image/Olia.jpg');
 
 --
 -- Индексы сохранённых таблиц
@@ -64,7 +68,7 @@ ALTER TABLE `organizations`
 -- AUTO_INCREMENT для таблицы `organizations`
 --
 ALTER TABLE `organizations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
