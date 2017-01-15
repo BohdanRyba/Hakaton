835de66587c9aa20e064439fb068b11e039fff01
RomaSlobodeniuk <romsl1991@gmail.com>
2017-01-15 16:03:13 +0200

    before merging

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -226,24 +226,17 @@ class AdminController
 
     public function actionDancingList()
     {
-//        echo 'Hello from dancing list!';
-
-        $db = Db::getConnection(Db::ADMIN_BASE);
-        $query = "SELECT * FROM `dance_groups` ORDER BY id DESC";
-        $result = $db->query($query);
-        $list = array();
-        $i = 0;
-        while ($row = $result->fetch_assoc()) {
-            $list[$i]['id'] = $row['id'];
-            $list[$i]['dance_group_name'] = $row['dance_group_name'];
-            $list[$i]['d_program'] = $row['d_program'];
-            $list[$i]['d_age_category'] = $row['d_age_category'];
-            $list[$i]['d_nomination'] = $row['d_nomination'];
-            $list[$i]['d_league'] = $row['d_league'];
-            $i++;
+        if(!empty($_POST)){
+            if (isset($_POST) && !empty($_POST['redirect'])) {
+                $json = json_decode($_POST['redirect'], true);
+                self::showArray($json);
+//                $result = (integer)AdminModel::saveDanceProgram($json, 'update_list');
+                echo '<br>';
+//                echo 'here is the result of the operation: ' . $result . '<br>';
+                echo '<br>';
+            }
         }
-
-        $db->close();
+        $list = AdminModel::getAllDanceGroups('list');
 //
 //        self::showArray($list);
 //
