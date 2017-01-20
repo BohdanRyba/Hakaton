f50ea30fb848827d67a42b45d35317652ad4d7b8
Zakhar17 <Rulito2000@gmail.com>
2017-01-20 23:05:21 +0200

    lalal

--- views/main/js/pick_categories_for_event.js
+++ views/main/js/pick_categories_for_event.js
@@ -34,66 +34,67 @@ jQuery(function($) {
     });
 
 
-//     $body.on('click', '.dancing-group-list-item-to-see', function(){
-//         var $categoriesList=$('#categories-list'),
-//             $danceGroups=$('#pick-dancing-group-parameter-to-see').children(),
-//             $searchedCategoriesForm=$('#show-searched-dancing-groups');
-//
-//         $danceGroups.each(function () {
-//             $(this).removeClass('picked-dancing-group');
-//         });
-//
-//         $(this).addClass('picked-dancing-group');
-//
-//         $categoriesList.css('display', 'block');
-//
-//         $searchedCategoriesForm.children().remove();
-//
-// //AJAX 2 function AJAX_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//
-//         function ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER($parameter) {
-//             var $name=$parameter.attr('data-name'),
-//                 obj={};
-//
-//             obj['name']=$name;
-//             obj['parameter']='d_c_program';
-//
-//             $.ajax({
-//                 type:"POST",
-//                 url:'ajax_showCategoriesAccordingToParameter',
-//                 data: obj,
-//                 success: function(msg) {
-//                     console.log('ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER (ajax2) has worked successfully!');
-//                     console.log(msg);
-//                     var msg=JSON.parse(msg);
-//                     console.log(msg[0]);
-//                     if (msg[0]==undefined) {$searchedCategoriesForm.append('<div class="dp-info-wrapper"><p class="dance-category-name">таких категорий нет</p></div>');}
-//                     for (var i=0; i<msg.length; i++) {
-//                         var category=msg[i],
-//                             program=category['d_c_program'],
-//                             ageCategory=category['d_c_age_category'],
-//                             nomination=category['d_c_nomination'],
-//                             league=category['d_c_league'],
-//                             code=category['extra_id'],
-//                             id=category['id'],
-//                             id_dance_group=category['id_dance_group'],
-//                             generalName=program+' '+ageCategory+' '+nomination+' '+league;
-//
-//                         if (code==0) {code=''}
-//
-//                         $searchedCategoriesForm.append('<div class="dp-info-wrapper" data-id="'+id+'" data-extraId="'+code+'" data-catagoryName="'+generalName+'"><div class="btn-group-sm flat" role="group"><button type="button" class="btn btn-success edit-button edit-categories-info btn-flat"><i class="fa fa-edit"></i></button> <button type="button" class="btn btn-danger delete-button delete-categories-info btn-flat"><i class="fa fa-trash"></i></button> </div><p class="dance-category-name">'+generalName+'</p><label>Код:<input disabled disabled type="text" name="dance-program-code" class="input-standard dancing-group-info-code" value="'+code+'"></label></div>');
-//
-//                     }
-//
-//                 },
-//                 error: function (msg) {
-//                     console.log('ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER (ajax2) has failed work!');
-//                 }
-//             })
-//
-//         }
-//
-//         ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER($(this));
-//
-//     });
+    $body.on('click', '.dancing-group-list-item-to-see', function(){
+        var $categoriesList=$('#categories-list'),
+            $danceGroups=$('#pick-dancing-group-parameter-to-see').children(),
+            $searchedCategoriesForm=$('#show-searched-dancing-groups');
+
+        $danceGroups.each(function () {
+            $(this).removeClass('picked-dancing-group');
+        });
+
+        $(this).addClass('picked-dancing-group');
+
+        $categoriesList.css('display', 'block');
+
+        $searchedCategoriesForm.children().remove();
+
+//AJAX 2 function AJAX_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        function ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER($parameter) {
+            var $name=$parameter.attr('data-name'),
+                obj={};
+
+            obj['name']=$name;
+            obj['parameter']='d_c_program';
+
+            $.ajax({
+                type:"POST",
+                url:'ajax_showCategoriesAccordingToParameter',
+                data: obj,
+                success: function(msg) {
+                    console.log('ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER (ajax2) has worked successfully!');
+                    console.log(msg);
+                    var msg=JSON.parse(msg);
+                    console.log(msg[0]);
+                    if (msg[0]==undefined) {$searchedCategoriesForm.append('<div class="dp-info-wrapper"><p class="dance-category-name">таких категорий нет</p></div>');}
+                    for (var i=0; i<msg.length; i++) {
+                        var category=msg[i],
+                            program=category['d_c_program'],
+                            ageCategory=category['d_c_age_category'],
+                            nomination=category['d_c_nomination'],
+                            league=category['d_c_league'],
+                            code=category['extra_id'],
+                            id=category['id'],
+                            id_dance_group=category['id_dance_group'],
+                            generalName=program+' '+ageCategory+' '+nomination+' '+league;
+
+                        if (code==0) {code=''}
+
+                        $searchedCategoriesForm.append('<div class="dp-info-wrapper" data-id="'+id+'" data-extraId="'+code+'" data-catagoryName="'+generalName+'"><div class="btn-group-sm flat" role="group"><button type="button" class="btn btn-success edit-button edit-categories-info btn-flat"><i class="fa fa-edit"></i></button> <button type="button" class="btn btn-danger delete-button delete-categories-info btn-flat"><i class="fa fa-trash"></i></button> </div><p class="dance-category-name">'+generalName+'</p><label>Код:<input disabled disabled type="text" name="dance-program-code" class="input-standard dancing-group-info-code" value="'+code+'"></label></div>');
+
+                    }
+
+                },
+                error: function (msg) {
+                    console.log(msg);
+                    console.log('ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER (ajax2) has failed work!');
+                }
+            })
+
+        }
+
+        ajax_THAT_ADDS_CATEGORIES_ACCORDING_TO_PARAMETER($(this));
+
+    });
 });
