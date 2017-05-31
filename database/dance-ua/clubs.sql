ded806586d5403d91e051f85cdfa99de1255e51b
RomaSlobodeniuk <romsl1991@gmail.com>
2017-05-31 19:51:43 +0300

    I've added password and number to the club and exported table

--- database/dance-ua/clubs.sql
+++ database/dance-ua/clubs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Час створення: Трв 31 2017 р., 18:35
--- Версія сервера: 10.0.30-MariaDB-30
--- Версія PHP: 7.1.5
+-- Хост: 127.0.0.1
+-- Время создания: Май 31 2017 г., 19:50
+-- Версия сервера: 10.1.9-MariaDB
+-- Версия PHP: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База даних: `livlegends`
+-- База данных: `hakaton_admin`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `clubs`
+-- Структура таблицы `clubs`
 --
 
 CREATE TABLE `clubs` (
@@ -43,33 +43,33 @@ CREATE TABLE `clubs` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Дамп даних таблиці `clubs`
+-- Дамп данных таблицы `clubs`
 --
 
 INSERT INTO `clubs` (`id`, `club_name`, `club_image`, `club_country`, `club_city`, `club_shief`, `club_number`, `club_mail`, `password`, `grant`, `active`, `org_id_for_club`, `coaches`) VALUES
 (1, 'Wither Club', 'views/main/img/club_img/roma.jpg', 'Ukraine', 'Khmelnytski', 'Roma Slobodeniuk', '380673800836', 'romsl@i.ua', '21232f297a57a5a743894a0e4a801fc3', 4, 1, 1, ''),
 (2, 'LivLegend', 'views/main/img/club_img/gena.jpg', 'Украина', 'Хмельницкий', 'Геннадий Федосов', '380671234567', 'gena@i.ua', 'e0a7bf6b3b6eb370828d29888d4805bc', 4, 1, 1, 'Дима&Саша&Petro&Sergey&Lesya&Kira'),
-(3, 'New club test', 'views/main/img/club_img/IMG_7846.JPG', 'Good mordor', 'Frozen', 'Merik', '0', 'rio@gmail.com', 'cc48ee4e3ba11933784f5327895a7bea', 1, 1, 1, 'Turok'),
+(3, 'New club test', 'views/main/img/club_img/IMG_7846.JPG', 'Good mordor', 'Frozen', 'Merik', '0971123344', 'rio@gmail.com', '99059631864310dd1421a6a432aa2c7c', 1, 1, 1, 'Turok'),
 (4, 'My new Club', 'views/main/img/club_img/men_summer_pants-1.jpg', 'Uganda', 'Golden Gates', 'Moa', '(098) 897-1212', 'title@gmail.com', 'e5fc07ae8cbbda0273495b50ff9b057e', 1, 1, 1, 'Tyldor&Rabok'),
 (5, 'Club', 'views/main/img/club_img/IMG_8386.JPG', 'Fortuna', 'Luck', 'Griobani luckery', '(067) 321-0011', 'clubdance@i.ua', 'a25e9b5b9b704b13af47bc17630e7a78', 1, 1, 3, 'Trener&Trener one');
 
 --
--- Індекси збережених таблиць
+-- Индексы сохранённых таблиц
 --
 
 --
--- Індекси таблиці `clubs`
+-- Индексы таблицы `clubs`
 --
 ALTER TABLE `clubs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `club_name` (`club_name`);
 
 --
--- AUTO_INCREMENT для збережених таблиць
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблиці `clubs`
+-- AUTO_INCREMENT для таблицы `clubs`
 --
 ALTER TABLE `clubs`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
