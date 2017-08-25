171dd54bc114f96f62557e2089ff1b9d6ee9ee66
Zakhar17 <Rulito2000@gmail.com>
2017-08-25 17:44:45 +0300

    d

--- views/main/js/eventProgramPrintDragInit.js
+++ views/main/js/eventProgramPrintDragInit.js
@@ -226,7 +226,7 @@ jQuery(function($) {
                         category["category"] = categories[i]["d_c_program"] + ' ' + categories[i]["d_c_age_category"] + ' ' + categories[i]["d_c_nomination"] + ' ' + categories[i]["d_c_league"];
                         category["id"] = categories[i]["id"];
                         category["data-category"] = categories[i]["data-category"];
-                        category["isMax"] = categories[i]["is_max"];
+                        category["isMax"] = parseInt(categories[i]["is_max"]);
                         categoriesToPush[parseInt(categories[i]["sort_order"])] = category;
                     }
 
